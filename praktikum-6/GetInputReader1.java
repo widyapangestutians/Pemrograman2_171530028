c3f25f80d7a0ccd40502945defe6326f7e15c21e
widyapangestutians <36910824+widyapangestutians@users.noreply.github.com>
2018-10-17 09:25:33 +0700

    Update GetInputReader1.java

--- praktikum-6/GetInputReader1.java
+++ praktikum-6/GetInputReader1.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class GetInputReader2{
+public class GetInputReader1{
     public static void main( String[] args ) throws IOException{
         BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
 
